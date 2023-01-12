93db1113b8021cc6121e09516f6a56a34773a4ba
harrisimon <harrisimon@gmail.com>
2023-01-11 21:09:04 -0500

    added card stack component

--- src/components/CardStack.js
+++ src/components/CardStack.js
@@ -1,30 +1,35 @@
-import React, { useState } from 'react'
-import { Card } from 'semantic-ui-react'
+import React from "react"
+import { Card } from "semantic-ui-react"
 
 const CardStack = (props) => {
-    const {weather, postList} = props
-    const [cards, setCards] = useState(weather.reviews)
-    console.log(weather, "in card stack")
-    let reviews
-    if (cards.length >= 1) {
-        // reviews = postList.map((review, index) => (
-        //     <Card key={index}>
-        //         <Card.Content>
-        //             <Card.Header>
-        //                 {new Date(review.createdAt).toLocaleString("en-us")}
-        //             </Card.Header>
-        //         </Card.Content>
-        //         <Card.Content className="review-text">
-        //             {review.review}
-        //         </Card.Content>
-        //         <Card.Content extra>by: {review.author.email}</Card.Content>
-        //     </Card>
-        // ))
-    } else {
-        reviews = <p>No posts yet... sign in to leave a post</p>
-    }
-    return (
-        {reviews}
-    )
+	const { postList } = props
+	// const [cards, setCards] = useState(weather.reviews)
+	// console.log(weather, "in card stack")
+	let reviews
+
+	// console.log("post list in stack", postList)
+	if (postList.length >= 1) {
+		reviews = postList.map((review, index) => (
+			<Card key={index}>
+				<Card.Content>
+					<Card.Header>
+						{new Date(review.createdAt).toLocaleString("en-us")}
+					</Card.Header>
+				</Card.Content>
+				<Card.Content className="review-text">
+					{review.review}
+				</Card.Content>
+				<Card.Content extra>by: {review.author.email}</Card.Content>
+			</Card>
+		))
+	} else {
+		reviews = <p>No posts yet... sign in to leave a post</p>
+	}
+	
+	return (
+		<>
+			{reviews}
+		</>
+	)
 }
 export default CardStack
--- src/components/Latest.js
+++ src/components/Latest.js
@@ -1,21 +1,12 @@
 import React, { useEffect, useState } from "react"
 import { getLatestWeather } from "../api/weather"
-import {
-	Button,
-	Container,
-	Grid,
-	Modal,
-	Card,
-	Form,
-
-} from "semantic-ui-react"
+import { Button, Container, Grid, Modal, Card, Form } from "semantic-ui-react"
 import TextareaCounter from "react-textarea-counter"
 import { submitPost } from "../api/weather"
-
+import CardStack from "./CardStack"
 
 const Latest = (props) => {
 	const { user, msgAlert } = props
-	
 
 	const [tempMeasure, changeTempMeasure] = useState(true)
 	const [weather, setWeather] = useState(null)
@@ -28,7 +19,6 @@ const Latest = (props) => {
 	let temp
 	let pressure
 	let humidity
-	let reviews
 	let posttime
 	let addPost
 
@@ -54,9 +44,9 @@ const Latest = (props) => {
 		e.preventDefault()
 
 		submitPost(user, weather._id, post)
+			// .then(setRefresh(true))
 			.then(setPostOpen(false))
-			.then(setRefresh(true))
-			
+
 			// .then("refresh submit", console.log(refresh))
 			.catch((error) => {
 				msgAlert({
@@ -64,11 +54,10 @@ const Latest = (props) => {
 					message: "Create Post Failure" + error,
 				})
 			})
+		setRefresh(true)
 	}
 
 	const handleChange = (e) => {
-		// console.log("weather",weather._id)
-
 		setPost((prevPost) => {
 			const updatedName = e.target.name
 			let updatedValue = e.target.value
@@ -123,25 +112,6 @@ const Latest = (props) => {
 		pressure = Math.round(weather.pressure * 100) / 100
 		humidity = <p>{Math.floor(weather.humidity * 100) / 100}</p>
 		posttime = <p>{time}</p>
-		// reviews = weather.review[0]
-		if (weather.reviews.length >= 1) {
-			reviews = postList.map((review, index) => (
-				<Card key={index}>
-					<Card.Content>
-						<Card.Header>
-							{new Date(review.createdAt).toLocaleString("en-us")}
-						</Card.Header>
-					</Card.Content>
-					<Card.Content className="review-text">
-						{review.review}
-					</Card.Content>
-					<Card.Content extra>by: {review.author.email}</Card.Content>
-				</Card>
-			))
-		} else {
-			reviews = <p>No posts yet... sign in to leave a post</p>
-		}
-		// review = <h3>{weather.reviews[0].review}</h3>
 	} else {
 		return <p>...loading</p>
 		// add in loading wheel
@@ -152,8 +122,7 @@ const Latest = (props) => {
 				<Grid.Row>
 					<Grid.Column width={16}>
 						<div className="temp-label">
-
-						<h4>Temp</h4>
+							<h4>Temp</h4>
 						</div>
 						<div className="reading">{temp}</div>
 					</Grid.Column>
@@ -178,7 +147,11 @@ const Latest = (props) => {
 
 			<div className="reviews">
 				<Card.Group centered>
-					<div className="reading">{reviews}</div>
+					<div className="reading">
+						{/* {reviews} */}
+
+						<CardStack weather={weather} postList={postList} />
+					</div>
 				</Card.Group>
 			</div>
 
@@ -206,26 +179,24 @@ const Latest = (props) => {
 						About Tiny Weather
 					</Modal.Header>
 					<Modal.Content>
-						
-							Tiny Weather is a site created by
-							<a href="https://harrison-simon.netlify.app/">
-								{" "}
-								Harrison Simon
-							</a>
-							. The data presented on the site are gathered from a
-							Raspberry Pi with a weather sensor which takes a
-							reading every half hour.
-							<br />
-							<h3>About the readings</h3>
-							The readings are taken from a{" "}
-							<a href="https://www.adafruit.com/product/2652">
-								BME 280 sensor
-							</a>
-							. Currently the humity sensor seems to be taking
-							incorrect readings.
-							<br />
-							More features coming soon...
-						
+						Tiny Weather is a site created by
+						<a href="https://harrison-simon.netlify.app/">
+							{" "}
+							Harrison Simon
+						</a>
+						. The data presented on the site are gathered from a
+						Raspberry Pi with a weather sensor which takes a reading
+						every half hour.
+						<br />
+						<h3>About the readings</h3>
+						The readings are taken from a{" "}
+						<a href="https://www.adafruit.com/product/2652">
+							BME 280 sensor
+						</a>
+						. Currently the humity sensor seems to be taking
+						incorrect readings.
+						<br />
+						More features coming soon...
 					</Modal.Content>
 				</Modal>
 			</div>
