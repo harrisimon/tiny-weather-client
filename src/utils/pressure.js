e62b5e5aac4fc95c4023eca292fdbc6094602a68
harrisimon <harrisimon@gmail.com>
2026-07-04 21:44:46 -0400

    changed humidity arc for gauge, max pressure

--- src/utils/pressure.js
+++ src/utils/pressure.js
@@ -12,7 +12,7 @@ export function formatInHg(hPa, decimals = 2) {
 
 const DEFAULT_PRESSURE_RANGE_INHG = {
 	minInHg: 29.0,
-	maxInHg: 30.6,
+	maxInHg: 30.5,
 }
 
 const PRESSURE_CHANGE_WINDOW_MS = 12 * 60 * 60 * 1000
